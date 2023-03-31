40912eeb109390a893b6241c1ad414cf39f18539
luizatorrens <luiza.torrens.2006@gmail.com>
2023-03-31 14:49:17 -0300

    Criação do Model: modelo do veículo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Marca, Categoria, Acessorio, Cor, Veiculo
+from .models import Marca, Categoria, Acessorio, Cor, Veiculo, Modelo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
 admin.site.register(Veiculo)
+admin.site.register(Modelo)
 
--- garagem/models.py
+++ garagem/models.py
@@ -5,7 +5,12 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
     def __str__(self):
         return self.nome.upper()
-
+    
+class Modelo(models.Model):
+    descricao= models.CharField(max_length=100)
+    def __str__(self):
+        return self.descricao
+    
 class Categoria(models.Model):
     descricao= models.CharField(max_length=100)
     def __str__(self):
@@ -15,18 +20,24 @@ class Acessorio(models.Model):
     descricao= models.CharField(max_length=100)
     def __str__(self):
         return self.descricao
+    class Meta:
+        verbose_name_plural= "Acessórios"
 
 class Cor(models.Model):
     descricao= models.CharField(max_length=100)
     def __str__(self):
         return self.descricao
+    class Meta:
+        verbose_name_plural= "Cores"
 
 class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=3, default=0, null=True, blank=True)
-    modelo = models.CharField(max_length=30, null=False, default=0)
     def __str__(self):
-        return f"modelo: {self.modelo}, marca: {self.marca}, ano: {self.ano}, cor: {self.cor}"
+        return f"marca: {self.marca}, modelo: {self.modelo}, ano: {self.ano}, cor: {self.cor}"
+    class Meta:
+        verbose_name_plural= "Veículos"
