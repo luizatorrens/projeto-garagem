5fec61c8b1df6ca217c09553f928a5b1987a5a63
luizatorrens <luiza.torrens.2006@gmail.com>
2023-04-28 14:41:33 -0300

    Criação do serializador para listagem do veículo

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -31,3 +31,14 @@ class VeiculoSerializer(ModelSerializer):
     class Meta:
         model = Veiculo
         fields = "__all__"
+
+class VeiculoDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+        depth = 1
+
+class VeiculoListSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ["id", "modelo", "marca", "cor"]
--- garagem/views.py
+++ garagem/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from garagem.models import Marca, Categoria, Cor, Acessorio, Modelo, Veiculo
-from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, AcessorioSerializer, ModeloSerializer, VeiculoSerializer
+from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, AcessorioSerializer, ModeloSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
@@ -26,5 +26,11 @@ class ModeloViewSet(ModelViewSet):
     serializer_class = ModeloSerializer
 
 class VeiculoViewSet(ModelViewSet):
-    queryset = Modelo.objects.all()
-    serializer_class = Veiculo
+    queryset = Veiculo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VeiculoListSerializer
+        elif self.action == "retrieve":
+            return VeiculoDetailSerializer
+        return VeiculoSerializer
