aae64e3c6575721252995b12b17373fe0d6f9aae
luizatorrens <luiza.torrens.2006@gmail.com>
2023-05-09 14:19:08 -0300

    Alteração nas urls

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,12 @@ from rest_framework.routers import DefaultRouter
 from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, ModeloViewSet, VeiculoViewSet
 
 router = DefaultRouter()
-router.register(r"marca", MarcaViewSet)
-router.register(r"categoria", CategoriaViewSet)
-router.register(r"cor", CorViewSet)
-router.register(r"acessorio", AcessorioViewSet)
-router.register(r"modelo", ModeloViewSet)
-router.register(r"veiculo", VeiculoViewSet)
+router.register(r"marcas", MarcaViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"modelos", ModeloViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
