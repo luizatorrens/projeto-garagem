e6eddabb35890124416700e035ddbc9987f9d87b
luizatorrens <luiza.torrens.2006@gmail.com>
2023-08-11 15:08:56 -0300

    Relacionamento n para n

--- garagem/models.py
+++ garagem/models.py
@@ -36,6 +36,7 @@ class Veiculo(models.Model):
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
+    acessorios = models.ManyToManyField(Acessorio, related_name="acessorios")
     preco = models.DecimalField(max_digits=7, decimal_places=3, default=0, null=True, blank=True)
     def __str__(self):
         return f"marca: {self.marca}, modelo: {self.modelo}, ano: {self.ano}, cor: {self.cor}"
